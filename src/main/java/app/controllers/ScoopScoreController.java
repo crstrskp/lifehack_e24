a5fa6d7eb9022270e2e3844d4eef206b742577bd
Gervig <gervig91@gmail.com>
2024-10-07 16:37:45 +0200

    logo er nu en gif. :joy: :ok_hand: :fire: :100:

--- src/main/java/app/controllers/ScoopScoreController.java
+++ src/main/java/app/controllers/ScoopScoreController.java
@@ -8,11 +8,11 @@ public class ScoopScoreController
 {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool)
     {
-        app.get("/projectname", ctx -> index(ctx, connectionPool));
+        app.get("/scoopscore", ctx -> index(ctx, connectionPool));
     }
 
     private static void index(Context ctx, ConnectionPool connectionPool)
     {
-        ctx.render("/projectname/index.html");
+        ctx.render("/scoopscore/index.html");
     }
 }
