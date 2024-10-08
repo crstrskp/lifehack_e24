b458c064e3adcecd8500de8907923ee877e47724
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-10-08 15:32:11 +0200

    optrettet mapper

---
+++ src/main/java/app/persistence/MotivationMapper.java
@@ -0,0 +1,44 @@
+package app.persistence;
+
+import app.entities.Motivation;
+
+import java.sql.*;
+import java.util.List;
+
+public class MotivationMapper {
+
+    public static void newMotivation(String motivationTitle, String motivationText, String imageURL, ConnectionPool connectionPool){
+
+    }
+
+    public static void deleteMotivation(int motivationId, ConnectionPool connectionPool){
+
+    }
+
+    public static Motivation getMotivation(ConnectionPool connectionPool){
+        String sql = "SELECT * FROM motivational_quotes ORDER BY RANDOM() LIMIT 1";
+        String title;
+        String text;
+        String imageURL;
+        int authorId;
+
+        try(Connection connection = connectionPool.getConnection()){
+            try(PreparedStatement ps = connection.prepareStatement(sql)){
+                ResultSet rs = ps.executeQuery();
+                title = rs.getString("title");
+                text = rs.getString("text");
+                imageURL = rs.getString("image_url");
+                authorId = rs.getInt("author_id");
+            }
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Motivation(authorId,title,text,imageURL);
+    }
+/*
+    public static List<Motivation> getMotivations(ConnectionPool connectionPool){
+
+    }*/
+}
