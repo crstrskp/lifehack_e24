517e3fcad7ecf2b2cc65c5f8361c1d00d4d55e6e
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-10-08 14:12:59 +0200

    jeg har lavet en forside til motivational.html hvor der er en login formular samt en knap der skal vise et tilfældigt citat fra db

--- src/main/java/app/entities/Motivation.java
+++ src/main/java/app/entities/Motivation.java
@@ -5,9 +5,11 @@ public class Motivation
     private String motivationTitle;
     private String  motivationText;
     private String  imageURL;
+    private int mmotivationId;
 
-    public Motivation(String motivationTitle, String motivationText, String imageURL)
+    public Motivation(int motivationId, String motivationTitle, String motivationText, String imageURL)
     {
+        this.mmotivationId = motivationId;
         this.motivationTitle = motivationTitle;
         this.motivationText = motivationText;
         this.imageURL = imageURL;
