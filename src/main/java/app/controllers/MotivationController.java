90eb3d5181bd3936c8beda02d2ffba16ff9227ca
AndyTheDragon <andre.jansam@gmail.com>
2024-10-08 15:07:05 +0200

    Added motivation variable to template

---
+++ src/main/java/app/controllers/MotivationController.java
@@ -0,0 +1,33 @@
+package app.controllers;
+
+import app.entities.Motivation;
+import app.persistence.ConnectionPool;
+import app.persistence.MotivationMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+
+public class MotivationController {
+
+    public static void addRoutes(Javalin app, ConnectionPool pool){
+        app.get("/motivational", ctx -> showMotivation(ctx,pool));
+
+    }
+
+
+    private void addMotivation(){
+
+    }
+
+
+    private static void showMotivation(Context ctx, ConnectionPool pool){
+
+        Motivation quote = MotivationMapper.getMotivation();
+        ctx.attribute("motivation", quote);
+
+        ctx.render("motivational/motivational.html");
+
+    }
+
+
+}
