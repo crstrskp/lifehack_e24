4921745bb03310f88b90d0cd5b9935d7290a40d3
Peter Bollhorn <pbollhorn@gmail.com>
2024-10-09 14:51:16 +0200

    Første bud på vores controller

--- src/main/java/app/controllers/ParmController.java
+++ src/main/java/app/controllers/ParmController.java
@@ -1,8 +1,14 @@
 package app.controllers;
 
+import app.entities.ParmWeightDTO;
+import app.entities.User;
+import app.exceptions.DatabaseException;
+import app.persistence.ParmWeightMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
+import java.util.ArrayList;
 
 
 public class ParmController {
@@ -15,16 +21,34 @@ public class ParmController {
 
     private static void showPage(Context ctx, ConnectionPool connectionPool) {
 
+        User currentUser = ctx.sessionAttribute("currentUser");
+        int user_id = currentUser.getUserId();
 
-
-
-        ctx.render("/parm/index.html");
+        try {
+            ArrayList<ParmWeightDTO> allWeights = ParmWeightMapper.getAllWeightPerUser(user_id, connectionPool);
+            ctx.attribute("allWeights", allWeights);
+            ctx.render("/parm/index.html");
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private static void postWeight(Context ctx, ConnectionPool connectionPool) {
 
         float weight = Float.parseFloat(ctx.formParam("weight"));
         System.out.println(weight);
+
+        User currentUser = ctx.sessionAttribute("currentUser");
+        int user_id = currentUser.getUserId();
+
+        try {
+            ParmWeightMapper.addWeight(user_id, weight, connectionPool);
+
+            showPage(ctx, connectionPool);
+
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
