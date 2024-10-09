ac65b85316dcd189c9a6988a351e012450e66a7b
MojoOno <Ffranck0406@gmail.com>
2024-10-09 11:26:45 +0200

    added owner_id with foreign key and refactored former owner_id to is_owner

--- src/main/resources/sql/eventplanner.sql
+++ src/main/resources/sql/eventplanner.sql
@@ -5,19 +5,25 @@
 CREATE TABLE IF NOT EXISTS public.eventplanner
 (
     event_id integer NOT NULL DEFAULT nextval('eventplanner_event_id_seq'::regclass),
-    owner_id boolean NOT NULL DEFAULT false,
+    is_owner boolean NOT NULL DEFAULT false,
     date_and_time character varying(20) COLLATE pg_catalog."default" NOT NULL,
     location character varying(50) COLLATE pg_catalog."default" NOT NULL,
     title character varying(20) COLLATE pg_catalog."default" NOT NULL,
     description character varying COLLATE pg_catalog."default",
     participants integer NOT NULL,
+    owner_id integer,
     CONSTRAINT eventplanner_pkey PRIMARY KEY (event_id),
+    CONSTRAINT owner_id FOREIGN KEY (owner_id)
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
     CONSTRAINT participants FOREIGN KEY (participants)
-    REFERENCES public.users (user_id) MATCH SIMPLE
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID
-    )
+        REFERENCES public.users (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
 
     TABLESPACE pg_default;
 
@@ -29,5 +35,5 @@ ALTER TABLE IF EXISTS public.eventplanner
 
 CREATE INDEX IF NOT EXISTS fki_participants
     ON public.eventplanner USING btree
-    (participants ASC NULLS LAST)
+        (participants ASC NULLS LAST)
     TABLESPACE pg_default;
