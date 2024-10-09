d6a39bcb45293cc056f0c950171094d6ea679a63
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-10-09 15:08:01 +0200

    MotivationMapper / oprettet tilføj, slet og get metoder.

--- src/main/java/app/persistence/MotivationMapper.java
+++ src/main/java/app/persistence/MotivationMapper.java
@@ -1,6 +1,8 @@
 package app.persistence;
 
 import app.entities.Motivation;
+import app.entities.User;
+import app.exceptions.DatabaseException;
 
 import java.sql.*;
 import java.util.List;
@@ -40,6 +42,75 @@ public class MotivationMapper {
         return null;
 
     }
+
+
+    public static Motivation getFavoriteMotivation(User user, ConnectionPool connectionPool){
+       String sql = "SELECT * FROM motivational_favorite AS f INNER JOIN motivational_quotes AS q ON f.motivation_id = q.id WHERE f.user_id = ?";
+       String title;
+       String text;
+       String imageURL;
+       int motivationId;
+
+       try (Connection connection = connectionPool.getConnection()){
+           try (PreparedStatement ps = connection.prepareStatement(sql)){
+               ps.setInt(1,user.getUserId());
+               ResultSet rs = ps.executeQuery();
+               if (rs.next()) {
+                   title = rs.getString("title");
+                   text = rs.getString("text");
+                   imageURL = rs.getString("image_url");
+                   motivationId = rs.getInt("id");
+                   return new Motivation(motivationId,title,text,imageURL);
+               }
+           }
+       }
+
+       catch (SQLException e) {
+           throw new RuntimeException(e);
+       }
+        return null;
+
+    }
+
+    public void addFavorite(User user, int motivationId, ConnectionPool connectionPool) throws DatabaseException
+    {
+        String sql = "INSERT INTO motivational_favorite (user_id, motivation_id) VALUES (?, ?)";
+        try (var connection = connectionPool.getConnection())
+        {
+            try (var prepareStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS))
+            {
+                prepareStatement.setInt(1, user.getUserId());
+                prepareStatement.setInt(2, motivationId);
+                prepareStatement.executeUpdate();
+                prepareStatement.getGeneratedKeys();
+
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Could not create user in the database");
+        }
+    }
+
+    public void deleteFromFavorite(int userId, int motivationId, ConnectionPool connectionPool) throws DatabaseException
+    {
+        String sql = "DELETE FROM motivational_favorite WHERE user_id = ? AND quote_id = ?";
+        try (var connection = connectionPool.getConnection())
+        {
+            try (var prepareStatement = connection.prepareStatement(sql))
+            {
+                prepareStatement.setInt(1, userId);
+                prepareStatement.setInt(2, motivationId);
+                prepareStatement.executeUpdate();
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Could not delete user from the database");
+        }
+    }
+
+
 /*
     public static List<Motivation> getMotivations(ConnectionPool connectionPool){
 
