d15262a19df94882689e86cb7f6eb8ca1098e942
Falcho <m.k.falcham@gmail.com>
2024-10-09 09:40:09 +0200

    Attributes, constructor, getters created

--- src/main/java/app/entities/Event.java
+++ src/main/java/app/entities/Event.java
@@ -1,5 +1,51 @@
 package app.entities;
 
-public class Event {
+import java.util.List;
 
+public class Event {
+    private int eventId;
+    private int ownerId;
+    private String dateAndTime;
+    private String location;
+    private String title;
+    private String description;
+    private List<User> participants;
+
+    public Event(int eventId, int ownerId, String dateAndTime, String location, String title, String description, List<User> participants) {
+        this.eventId = eventId;
+        this.ownerId = ownerId;
+        this.dateAndTime = dateAndTime;
+        this.location = location;
+        this.title = title;
+        this.description = description;
+        this.participants = participants;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<User> getParticipants() {
+        return participants;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDateAndTime() {
+        return dateAndTime;
+    }
 }
