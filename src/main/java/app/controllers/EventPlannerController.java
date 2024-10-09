55223b6988ae835aff4cc2706b64aeed16377e94
Falcho <m.k.falcham@gmail.com>
2024-10-09 14:11:41 +0200

    Created LeaveEvent method in controller and mapper, aswell as added routing in controller.

--- src/main/java/app/controllers/EventPlannerController.java
+++ src/main/java/app/controllers/EventPlannerController.java
@@ -1,43 +1,37 @@
 package app.controllers;
 
+import app.entities.User;
 import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 import app.persistence.EventPlannerMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
-public class EventPlannerController
-{
-    public static void addRoutes(Javalin app, ConnectionPool connectionPool)
-    {
+public class EventPlannerController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/eventplanner", ctx -> index(ctx, connectionPool));
         app.post("/eventplanner/create", ctx -> createEvent(ctx, connectionPool));
         app.post("/eventplanner/delete", ctx -> deleteEvent(ctx, connectionPool));
+        app.post("/eventplanner/leave", ctx -> leaveEvent(ctx, connectionPool));
     }
 
-    private static void index(Context ctx, ConnectionPool connectionPool)
-    {
+    private static void index(Context ctx, ConnectionPool connectionPool) {
         ctx.render("/eventplanner/index.html");
     }
 
-    private static void createEvent(Context ctx, ConnectionPool connectionPool)
-    {
+    private static void createEvent(Context ctx, ConnectionPool connectionPool) {
         Boolean ownerId = Boolean.valueOf(ctx.formParam("ownerid"));
         String eventDate = ctx.formParam("eventdateandtime");
         String eventName = ctx.formParam("eventname");
         String eventLocation = ctx.formParam("eventlocation");
         String eventDescription = ctx.formParam("eventdescription");
 
-        if (eventName.length() > 3)
-        {
-            try
-            {
+        if (eventName.length() > 3) {
+            try {
                 EventPlannerMapper.createEvent(ownerId, eventDate, eventName, eventLocation, eventDescription, connectionPool);
                 ctx.attribute("message", "Event created.");
                 ctx.render("/eventplanner/index.html");
-            }
-            catch (DatabaseException e)
-            {
+            } catch (DatabaseException e) {
                 ctx.attribute("message", "Something went wrong, Try again");
                 ctx.render("/eventplanner/index.html");
             }
@@ -47,7 +41,7 @@ public class EventPlannerController
         }
     }
 
-    public static void deleteEvent (Context ctx, ConnectionPool connectionPool){
+    public static void deleteEvent(Context ctx, ConnectionPool connectionPool) {
         int eventId = Integer.parseInt(ctx.formParam("eventid"));
         try {
             EventPlannerMapper.deleteEvent(eventId, connectionPool);
@@ -55,6 +49,25 @@ public class EventPlannerController
             ctx.render("/eventplanner/index.html");
         } catch (DatabaseException e) {
             ctx.attribute("message", "Something went wrong, Try again");
+            ctx.render("/eventplanner/index.html");
+        }
+    }
+
+    public static void leaveEvent(Context ctx, ConnectionPool connectionPool) {
+        int eventId = Integer.parseInt(ctx.formParam("eventid"));
+        User currentUser = ctx.sessionAttribute("currentUser");
+
+        if(currentUser != null) {
+            try {
+                EventPlannerMapper.leaveEvent(eventId, currentUser, connectionPool);
+                ctx.attribute("message", "You have left the event.");
+                ctx.render("/eventplanner/index.html");
+            } catch (DatabaseException e) {
+                ctx.attribute("message", "Something went wrong, Try again");
+                ctx.render("/eventplanner/index.html");
+            }
+        } else {
+            ctx.attribute("message", "You must be logged in to leave an event.");
             ctx.render("/eventplanner/index.html");
         }
     }
--- src/main/java/app/persistence/EventPlannerMapper.java
+++ src/main/java/app/persistence/EventPlannerMapper.java
@@ -10,58 +10,66 @@ import java.sql.SQLException;
 
 public class EventPlannerMapper {
 
-        public static void createEvent(Boolean ownerId, String dateAndTime, String location, String title , String decription, ConnectionPool connectionPool) throws DatabaseException
-        {
-            String sql = "insert into eventplanner (ownerid, dateandtime, location, title, decription) values (true, ?,?,?,?)";
+    public static void createEvent(Boolean ownerId, String dateAndTime, String location, String title, String decription, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "insert into eventplanner (ownerid, dateandtime, location, title, decription) values (true, ?,?,?,?)";
 
-            try (
-                    Connection connection = connectionPool.getConnection();
-                    PreparedStatement ps = connection.prepareStatement(sql)
-            )
-            {
-                ps.setBoolean(1, ownerId);
-                ps.setString(2, dateAndTime);
-                ps.setString(3, location);
-                ps.setString(4, title);
-                ps.setString(5, decription);
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setBoolean(1, ownerId);
+            ps.setString(2, dateAndTime);
+            ps.setString(3, location);
+            ps.setString(4, title);
+            ps.setString(5, decription);
 
-                int rowsAffected = ps.executeUpdate();
-                if (rowsAffected != 1)
-                {
-                    throw new DatabaseException("Fejl ved oprettelse af event");
-                }
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved oprettelse af event");
             }
-            catch (SQLException e)
-            {
-                String msg = "Der er sket en fejl. Prøv igen";
-                if (e.getMessage().startsWith("ERROR: duplicate key value "))
-                {
-                    msg = "Eventet findes allerede. Vælg et andet";
-                }
-                throw new DatabaseException(msg, e.getMessage());
+        } catch (SQLException e) {
+            String msg = "Der er sket en fejl. Prøv igen";
+            if (e.getMessage().startsWith("ERROR: duplicate key value ")) {
+                msg = "Eventet findes allerede. Vælg et andet";
             }
-        }
-
-        public static void deleteEvent(int eventId, ConnectionPool connectionPool) throws DatabaseException
-        {
-            String sql = "delete from eventplanner where eventid=?";
-
-            try (
-                    Connection connection = connectionPool.getConnection();
-                    PreparedStatement ps = connection.prepareStatement(sql)
-            )
-            {
-                ps.setInt(1, eventId);
-
-                int rowsAffected = ps.executeUpdate();
-                if (rowsAffected != 1)
-                {
-                    throw new DatabaseException("Fejl ved sletning af event");
-                }
-            }
-            catch (SQLException e)
-            {
-                throw new DatabaseException("Der er sket en fejl. Prøv igen", e.getMessage());
-            }
+            throw new DatabaseException(msg, e.getMessage());
         }
     }
+
+    public static void deleteEvent(int eventId, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "delete from eventplanner where eventid=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setInt(1, eventId);
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved sletning af event");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Der er sket en fejl. Prøv igen", e.getMessage());
+        }
+    }
+
+    public static void leaveEvent (int eventId, User user, ConnectionPool connectionPool) throws DatabaseException {
+        String sql = "delete from eventplanner where eventid=? and userid=?";
+
+        try (
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql)
+        ) {
+            ps.setInt(1, eventId);
+            ps.setInt(2, user.getUserId());
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1) {
+                throw new DatabaseException("Fejl ved forlade event");
+            }
+        } catch (SQLException e) {
+            throw new DatabaseException("Der er sket en fejl. Prøv igen", e.getMessage());
+        }
+    }
+}
