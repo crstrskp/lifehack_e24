ca4ea54038a0b32b0a9abf360253f63ded5e20f7
Gervig <gervig91@gmail.com>
2024-10-09 15:50:17 +0200

    Update RankListMapper.java

--- src/main/java/app/persistence/ScoopScore/RankListMapper.java
+++ src/main/java/app/persistence/ScoopScore/RankListMapper.java
@@ -1,32 +1,48 @@
 package app.persistence.ScoopScore;
 
 import app.entities.SS.RankList;
+import app.entities.SS.RankListItem;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RankListMapper
 {
-    public static List<RankList> getAllPublicRankList(boolean is_public, ConnectionPool connectionPool)
+    public static List<RankList> getAllPublicRankList(boolean isPublic, ConnectionPool connectionPool) throws DatabaseException
     {
         List<RankList> rankLists = new ArrayList<>();
         String sql = "SELECT * FROM ss_rank_list WHERE is_public=true";
 
         try (
-                ConnectionPool connection = connectionPool.getConnection();
-                PreparedStatement ps = connection.getConnection().prepareStatement(sql);
+                Connection connection = connectionPool.getConnection();
+                PreparedStatement ps = connection.prepareStatement(sql);
                 )
         {
-            ps.setBoolean(1, is_public);
+            ps.setBoolean(1, isPublic);
             ResultSet rs = ps.executeQuery();
             while(rs.next())
             {
-                int id = rs.getInt()
+                List<RankListItem> rankListItemPerUser = new ArrayList<>();
+                int id = rs.getInt("id");
+                int user_id = rs.getInt("user_id");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                boolean is_public = rs.getBoolean("is_public");
+                //TODO getRankListItems from user_id
+//                rankListItemPerUser = RankListItemMapper.getRankListItems();
+                rankLists.add(new RankList(id, user_id, title, description, is_public));
             }
-
         }
+        catch (SQLException e)
+        {
+            throw new DatabaseException("Fejl!", e.getMessage());
+        }
+        return rankLists;
     }
 }
