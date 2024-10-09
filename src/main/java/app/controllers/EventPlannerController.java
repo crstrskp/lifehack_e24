ef8ef550d9e025209b2fd46921f958191b303438
Falcho <m.k.falcham@gmail.com>
2024-10-09 11:24:00 +0200

    Matched controller and mapper attributes

--- src/main/java/app/controllers/EventPlannerController.java
+++ src/main/java/app/controllers/EventPlannerController.java
@@ -20,6 +20,7 @@ public class EventPlannerController
 
     private static void createEvent(Context ctx, ConnectionPool connectionPool)
     {
+        Boolean ownerId = Boolean.valueOf(ctx.formParam("ownerid"));
         String eventDate = ctx.formParam("eventdateandtime");
         String eventName = ctx.formParam("eventname");
         String eventLocation = ctx.formParam("eventlocation");
@@ -29,7 +30,7 @@ public class EventPlannerController
         {
             try
             {
-                EventPlannerMapper.createEvent(eventDate, eventName, eventLocation, eventDescription, connectionPool);
+                EventPlannerMapper.createEvent(ownerId, eventDate, eventName, eventLocation, eventDescription, connectionPool);
                 ctx.attribute("message", "Event created.");
                 ctx.render("/eventplanner/index.html");
             }
