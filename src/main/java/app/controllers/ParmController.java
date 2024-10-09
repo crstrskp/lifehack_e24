483d55fd42a7fdbeefc56c3a682f7e411d5a5a3a
Peter Bollhorn <pbollhorn@gmail.com>
2024-10-09 11:38:45 +0200

    before pushing to github

--- src/main/java/app/controllers/ParmController.java
+++ src/main/java/app/controllers/ParmController.java
@@ -4,15 +4,20 @@ import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
+
 public class ParmController {
 
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("/parm", ctx -> index(ctx, connectionPool));
+        app.get("/parm", ctx -> showPage(ctx, connectionPool));
         app.post("/parm", ctx -> postWeight(ctx, connectionPool));
     }
 
-    private static void index(Context ctx, ConnectionPool connectionPool) {
+    private static void showPage(Context ctx, ConnectionPool connectionPool) {
+
+
+
+
         ctx.render("/parm/index.html");
     }
 
