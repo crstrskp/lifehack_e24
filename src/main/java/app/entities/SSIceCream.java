5ed0f0db9dad63f485ee21809a5192388f7c95f6
Crispy212 <Christofferleisted@gmail.com>
2024-10-09 11:54:58 +0200

    Added SSIceCream.java under entities

--- src/main/java/app/entities/SSIceCream.java
+++ src/main/java/app/entities/SSIceCream.java
@@ -5,10 +5,24 @@ public class SSIceCream {
     private int id;
     private String name;
 
-}
+    public SSIceCream(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 
-public Icecream(int id, String name){
+    public int getId() {
+        return id;
+    }
 
+    public String getName() {
+        return name;
+    }
 
-
+    @Override
+    public String toString() {
+        return "SSIceCream{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
