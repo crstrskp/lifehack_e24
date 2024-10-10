1b74395f11ad4e206ed4ccfaa39157b20bdb3168
Rolf <158441656+mrPrimeBeef@users.noreply.github.com>
2024-10-10 10:40:34 +0200

    sql fil fungere

--- src/main/resources/sql/parm/parmweight.sql
+++ src/main/resources/sql/parm/parmweight.sql
@@ -1,19 +1,22 @@
--- Table: public.parmweight
+DROP TABLE IF EXISTS public.parmweight;
 
--- DROP TABLE IF EXISTS public.parmweight;
+
+CREATE SEQUENCE public.parmweight_weight_id_seq;
+
 
 CREATE TABLE IF NOT EXISTS public.parmweight
 (
-    weight_id SERIAL PRIMARY KEY,
+    weight_id integer NOT NULL DEFAULT nextval('parmweight_weight_id_seq'),
     user_id integer NOT NULL,
     weight real NOT NULL,
-    date timestamp without time zone NOT NULL,
+    date timestamp without time zone NOT NULL DEFAULT CURRENT_DATE,
+    CONSTRAINT parmweight_pkey PRIMARY KEY (weight_id),
     CONSTRAINT fk_userid FOREIGN KEY (user_id)
     REFERENCES public.users (user_id)
-    ON UPDATE NO ACTION
-    ON DELETE NO ACTION
-    NOT VALID
+                   ON UPDATE NO ACTION
+                   ON DELETE NO ACTION
     )
+
     TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.parmweight
