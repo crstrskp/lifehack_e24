81acfd90e64a6a7c69b8ac1c1384b4fc4ed35398
Peter Bollhorn <pbollhorn@gmail.com>
2024-10-10 10:58:12 +0200

    Controller and Mappers work together

--- src/main/java/app/controllers/ParmController.java
+++ src/main/java/app/controllers/ParmController.java
@@ -7,15 +7,10 @@ import app.persistence.ParmWeightMapper;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+
 import java.util.ArrayList;
 
-// TODO: Delete theses imports
-import java.util.Calendar;
-import java.util.Date;
-
-
 public class ParmController {
-
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/parm", ctx -> showPage(ctx, connectionPool));
@@ -27,36 +22,17 @@ public class ParmController {
         User currentUser = ctx.sessionAttribute("currentUser");
         int user_id = currentUser.getUserId();
 
-        // TODO: Delete this code
-        Calendar calendar = Calendar.getInstance();
+        try {
+            ArrayList<ParmWeightDTO> weightList = ParmWeightMapper.getAllWeightPerUser(user_id, connectionPool);
 
-        ArrayList<ParmWeightDTO> weightList = new ArrayList<ParmWeightDTO>();
-        calendar.set(2024, Calendar.SEPTEMBER, 4);
-        Date specificDate = calendar.getTime();
-        weightList.add(new ParmWeightDTO(4,user_id,(float)78,specificDate));
-        calendar.set(2024, Calendar.SEPTEMBER, 3);
-        specificDate = calendar.getTime();
-        weightList.add(new ParmWeightDTO(3,user_id,(float)79,specificDate));
-        calendar.set(2024, Calendar.SEPTEMBER, 2);
-        specificDate = calendar.getTime();
-        weightList.add(new ParmWeightDTO(2,user_id,(float)80,specificDate));
-        calendar.set(2024, Calendar.SEPTEMBER, 1);
-        specificDate = calendar.getTime();
-        weightList.add(new ParmWeightDTO(1,user_id,(float)81,specificDate));
+            for (ParmWeightDTO dto : weightList)
+                System.out.println(dto);
 
-        for(ParmWeightDTO dto: weightList)
-            System.out.println(dto);
-
-        ctx.attribute("weightList", weightList);
-        ctx.render("/parm/index.html");
-
-//        try {
-//            ArrayList<ParmWeightDTO> weightList = ParmWeightMapper.getAllWeightPerUser(user_id, connectionPool);
-//            ctx.attribute("weightList", weightList);
-//            ctx.render("/parm/index.html");
-//        } catch (DatabaseException e) {
-//            throw new RuntimeException(e);
-//        }
+            ctx.attribute("weightList", weightList);
+            ctx.render("/parm/index.html");
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -67,15 +43,15 @@ public class ParmController {
 
         User currentUser = ctx.sessionAttribute("currentUser");
         int user_id = currentUser.getUserId();
-        System.out.println("user_id: "+user_id);
+        System.out.println("user_id: " + user_id);
 
-//        try {
-//            ParmWeightMapper.addWeight(user_id, weight, connectionPool);
+        try {
+            ParmWeightMapper.addWeight(user_id, weight, connectionPool);
             showPage(ctx, connectionPool);
 
-//        } catch (DatabaseException e) {
-//            throw new RuntimeException(e);
-//        }
+        } catch (DatabaseException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
